4263dbe9dc845a9918be54bc2fd5953c8d4eb006
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-05-22 04:37:24 +0500

    Home Page Content Merging

--- client/src/components/ChooseUsSection.jsx
+++ client/src/components/ChooseUsSection.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import banner from '../assets/sideban.png'
+import React from "react";
+import banner from "../assets/sideban.png";
 function ChooseUsSection() {
   return (
-    <div className='flex justify-center  gap-x-24 py-40 '>
-        <div className='w-[30%] pt-28'>
-        <h2 className='text-theme-yellow text-4xl py-8'>Why Choose Us</h2>
-        <h1 className='text-theme-red text-6xl pb-4 font-bold leading-tight'>SAY GOODBYE TO WRITER'S BLOCK.</h1>
-        <p className='text-2xl leading-snug'>Finish your resume faster with <span className='text-theme-red'> thousands of pre-written examples </span> for hundreds of jobs you can insert directly into your resume!</p>
-        <button className='bg-theme-red overflow-hidden w-1/3 p-6 text-2xl text-white rounded-2xl mt-24 before:transition-all before:duration-[1200ms] relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-yellow before:top-0 before:left-0  before:translate-x-[-125%]  hover:before:translate-x-[0%] before:rounded-2xl'>Get Started</button>
-        </div>
-        <div className='shadow-2xl'>
-            <img src={banner}  alt="" />
-        </div>
+    <div className="flex justify-center gap-x-24 py-40 ">
+      <div className="w-[30%] pt-28">
+        <h2 className="text-theme-yellow text-4xl py-8">Why Choose Us</h2>
+        <h1 className="text-theme-red text-6xl pb-4 font-bold leading-tight">
+          SAY GOODBYE TO WRITER'S BLOCK.
+        </h1>
+        <p className="text-2xl leading-snug">
+          Finish your resume faster with{" "}
+          <span className="text-theme-red">
+            {" "}
+            thousands of pre-written examples{" "}
+          </span>{" "}
+          for hundreds of jobs you can insert directly into your resume!
+        </p>
+        <button className='bg-theme-red overflow-hidden w-1/3 p-6 text-2xl text-white rounded-2xl mt-24 before:transition-all before:duration-[1200ms] relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-yellow before:top-0 before:left-0  before:translate-x-[-125%]  hover:before:translate-x-[0%] before:rounded-2xl'>
+          Get Started
+        </button>
+      </div>
+      <div className="shadow-2xl">
+        <img src={banner} alt="" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default ChooseUsSection
+export default ChooseUsSection;
--- client/src/components/WorksSection.jsx
+++ client/src/components/WorksSection.jsx
@@ -1,48 +1,62 @@
-import React from 'react'
-import research from '../assets/research.png'
-import searchicon from '../assets/digital-marketing.png'
-import blueprint from '../assets/blueprint.png'
+import React from "react";
+import research from "../assets/research.png";
+import searchicon from "../assets/digital-marketing.png";
+import blueprint from "../assets/blueprint.png";
 function WorksSection() {
   return (
-    <div className='bg-coral-light w-full h-full '>
-<div className='text-center flex py-24 justify-center '>
- <div className='w-1/4'>
- <h2 className='text-theme-yellow text-3xl mb-2'>How It Works</h2>
-  <p className='text-theme-red leading-tight  text-5xl font-bold'>Build your job-winning <br /> resume with AI assistance in three simple steps</p>
- </div>
-</div>
-<div className='flex justify-center pb-16'>
-<div className='bg-white group/card overflow-hidden w-1/5 before:transition-all before:duration-[1200ms] text-start mb-10 mr-12 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%] '>
-  <div className='pt-14 px-6' >
-    <img  className='h-36' src={searchicon} alt="" />
-  </div>
-  <div>
-    <h1 className='text-4xl font-bold text-theme-red group-hover/card:text-white py-6 px-6'>Step O1</h1>
-    <h2 className='text-2xl text-theme-black  group-hover/card:text-white leading-tight pb-14 px-6'>Choose one of our professionally designed resumes.</h2>
-  </div>
-</div>
-<div className='bg-white w-1/5 group/card overflow-hidden before:transition-all before:duration-[1200ms]  text-start mb-10 mr-12 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%]'>
-  <div className='pt-14 px-6' >
-    <img  className='h-36' src={research} alt="" />
-  </div>
-  <div>
-    <h1 className='text-4xl  group-hover/card:text-white font-bold text-theme-red py-6 px-6'>Step O2</h1>
-    <h2 className='text-2xl  group-hover/card:text-white text-theme-black leading-tight pb-14 px-6'>Fill in your details using our simple prompts.</h2>
-  </div>
-</div>
-<div className='bg-white w-1/5 group/card overflow-hidden before:transition-all before:duration-[1200ms]  text-start mb-10 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%]'>
-  <div className='pt-14' >
-    <img  className='h-36 px-6' src={blueprint} alt="" />
-  </div>
-  <div>
-    <h1 className='text-4xl  group-hover/card:text-white font-bold text-theme-red py-6 px-6'>Step O3</h1>
-    <h2 className='text-2xl  group-hover/card:text-white text-theme-black leading-tight pb-14 px-6'>Download & start applying for jobs today!</h2>
-  </div>
-</div>
-</div>
-        
+    <div className="bg-coral-light w-full h-full ">
+      <div className="text-center flex py-24 justify-center ">
+        <div className="w-[60%]">
+          <h2 className="text-theme-yellow text-3xl mb-2">How It Works</h2>
+          <p className="text-theme-red leading-tight text-5xl font-bold">
+            Build your job-winning <br /> resume with AI assistance in three
+            simple steps
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center pb-16">
+        <div className='bg-white group/card overflow-hidden w-1/5 before:transition-all before:duration-[1200ms] text-start mb-10 mr-12 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%] '>
+          <div className="pt-14 px-6">
+            <img className="h-36" src={searchicon} alt="" />
+          </div>
+          <div>
+            <h1 className="text-4xl font-bold text-theme-red group-hover/card:text-white py-6 px-6">
+              Step O1
+            </h1>
+            <h2 className="text-2xl text-theme-black  group-hover/card:text-white leading-tight pb-14 px-6">
+              Choose one of our professionally designed resumes.
+            </h2>
+          </div>
+        </div>
+        <div className='bg-white w-1/5 group/card overflow-hidden before:transition-all before:duration-[1200ms]  text-start mb-10 mr-12 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%]'>
+          <div className="pt-14 px-6">
+            <img className="h-36" src={research} alt="" />
+          </div>
+          <div>
+            <h1 className="text-4xl  group-hover/card:text-white font-bold text-theme-red py-6 px-6">
+              Step O2
+            </h1>
+            <h2 className="text-2xl  group-hover/card:text-white text-theme-black leading-tight pb-14 px-6">
+              Fill in your details using our simple prompts.
+            </h2>
+          </div>
+        </div>
+        <div className='bg-white w-1/5 group/card overflow-hidden before:transition-all before:duration-[1200ms]  text-start mb-10 relative z-[10] before:content-[""] before:absolute before:z-[-1] before:w-full before:h-full before:bg-theme-red  before:translate-x-[-101%]  hover:before:translate-x-[0%]'>
+          <div className="pt-14">
+            <img className="h-36 px-6" src={blueprint} alt="" />
+          </div>
+          <div>
+            <h1 className="text-4xl  group-hover/card:text-white font-bold text-theme-red py-6 px-6">
+              Step O3
+            </h1>
+            <h2 className="text-2xl  group-hover/card:text-white text-theme-black leading-tight pb-14 px-6">
+              Download & start applying for jobs today!
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WorksSection
+export default WorksSection;
