0082486266971dd79d44feaaa4a8c3dc51ea8ae0
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-05-22 23:54:53 +0500

    nabdev some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,15 @@ import React from "react";
 // Import Pages
 // import Home from "./pages/Home";
 import Resume from "./pages/Resume";
-import CoverLetter from "./pages/CoverLetter";
+// import CoverLetter from "./pages/CoverLetter";
 
 const App = () => {
   return (
     <>
       <div className="w-full">
         {/* <Home /> */}
-        {/* <Resume /> */}
-        <CoverLetter />
+        <Resume />
+        {/* <CoverLetter /> */}
       </div>
     </>
   );
