c9c162c5aa77d3cab2517554285b71a96af55269
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-05-23 03:36:28 +0500

    TemplateCard, ratingStars, & badge

---
+++ client/src/components/RatingStars.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+
+export default function BasicRating({ rating }) {
+    const [value, setValue] = React.useState(rating);
+
+    return (
+        <Box
+            sx={{
+                '& > legend': { mt: 2 },
+                '& .css-1qqgbpl-MuiRating-root': {fontSize: '2.3rem'}
+            }}
+        >
+            <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                    setValue(newValue);
+                }}
+            />
+        </Box>
+    );
+}
