95287f5eb90b6de9da2547e01a78733c7339694c
Muhammad Nabeel <nabeelmajeed266@gmail.com>
2024-05-23 23:20:00 +0500

    Dashboard Sidebar and Navbar Comptete

--- client/src/components/Dashboard/DashSidebar.jsx
+++ client/src/components/Dashboard/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DashSidebar = () => {
-  return <div>DashSidebar</div>;
+  return <div className="w-[20%] bg-blue-300">DashSidebar</div>;
 };
 
 export default DashSidebar;
